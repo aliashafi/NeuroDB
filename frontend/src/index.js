5a60086157b5e73120e4cd32c3b0c5203a212679
aliashafi <alia_shafi@alumni.brown.edu>
2019-09-23 08:44:35 -0700

    edited index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import axios from 'axios'
-import App from './App';
-import Root from './components/Root'
-import * as serviceWorker from './serviceWorker';
-import configureStore from './store/store'
-ReactDOM.render(<App />, document.getElementById('root'));
+import Root from './components/Root';
+import configureStore from './store/store';
+import jwt_decode from 'jwt-decode';
+import { setAuthToken } from './util/session_api_util';
+// import { logout } from './actions/session_actions';
+
+document.addEventListener('DOMContentLoaded', () => {
     let store;
+
     // if (localStorage.jwtToken) {
-    //     // setAuthToken(localStorage.jwtToken);
-    //     // const decodedUser = jwt_decode(localStorage.jwtToken);
-    //     // const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
-
+        // setAuthToken(localStorage.jwtToken);
+        // const decodedUser = jwt_decode(localStorage.jwtToken);
+        // const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
         // store = configureStore(preloadedState);
-
-    //     const currentTime = Date.now() / 1000;
-
-    //     // if (decodedUser.exp < currentTime) {
-    //     //     // store.dispatch(logout());
-    //     //     // window.location.href = '/login';
-    //     // }
+        // const currentTime = Date.now() / 1000;
+        // if (decodedUser.exp < currentTime) {
+            // store.dispatch(logout());
+            // window.location.href = '/login';
+        // }
     // } else {
-        store = configureStore({});
+        // store = configureStore({});
     // }
     const root = document.getElementById('root');
 
-    ReactDOM.render(<Root store={store} />, root);
-serviceWorker.unregister();
+    ReactDOM.render(<div className="splash-page">Hello</div>, root);
+});
