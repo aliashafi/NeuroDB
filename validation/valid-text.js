e940ba35f81bb07e7b94dcc9c9da8fbc2423deed
ernestman <ernestjunman@gmail.com>
2019-09-22 17:32:17 -0700

    fixing, added twitter on accident

--- validation/valid-text.js
+++
@@ -1,8 +0,0 @@
-// validation for checking length of input
-
-const validText = (str) => {
-    // .trim() removes spaces
-    return typeof str === "string" && str.trim().length > 0;
-}
-
-module.exports = validText;
