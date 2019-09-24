765b8df2f0e07227901a5243cbd2dde5d74b4997
dollyshin <dollyshin88@gmail.com>
2019-09-24 13:28:18 -0700

    Add subcomponents necessary for patient show page

--- frontend/src/reducers/patient_reducer.js
+++ frontend/src/reducers/patient_reducer.js
@@ -5,7 +5,7 @@ export default (state = {}, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_PATIENT: // Does this have the data key array?????????
-            return merge({}, { [action.patient._id]: action.patient });
+            return merge({}, { [action.patient.data._id]: action.patient.data });
         case RECEIVE_PATIENTS:
             let newState = {};
             action.patients.data.forEach(patient => ( newState[patient._id]= patient ));
