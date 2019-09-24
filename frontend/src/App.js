eb012b86fd65645cc6a26779215008f23d0dfbab
dollyshin <dollyshin88@gmail.com>
2019-09-24 12:42:04 -0700

    Create basic component structure and layout for patient show page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import SplashPage from './components/splash_page'
+import SplashPage from './components/splash_page';
+import PatientShowContainer from './components/Patients/patient_show_container';
 // import logo from './logo.svg';
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={SplashPage} />
+        <Route path='/show' component={PatientShowContainer} />
       </Switch>
     </div>
   );
---
+++ frontend/src/components/Patients/PatientShowSubcomp/patient_show_imaging_data.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class PatientShowImagingData extends React.Component {
+
+    render() {
+        return (
+            <div className='patient-show-inner-card'>
+                <div className='inner-card__field-grouping'>
+                    <div></div>
+                    <div></div>
+                </div>
+
+            </div>
+        );
+    }
+
+}
+
+export default PatientShowImagingData;
