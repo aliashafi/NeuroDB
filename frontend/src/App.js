6a9bb7bc2c86c0c3fcdfb6eaffaf353df8cb6872
ernestman <ernestjunman@gmail.com>
2019-09-24 18:29:53 -0700

    resolving conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,15 @@
 import React from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-=======
 import { Switch, Route } from 'react-router-dom';
 import SplashPage from './components/splash_page'
 // import logo from './logo.svg';
->>>>>>> 40da76dd655bf920afcd21b2595bc0b4b352a821
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <Switch>
         <Route exact path="/" component={SplashPage} />
       </Switch>
->>>>>>> 40da76dd655bf920afcd21b2595bc0b4b352a821
     </div>
   );
 }
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import {createStore, applyMiddleware} from "redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
-
-import RootReducer from "../reducers/root_reducer";
-
-const configureStore = (preloadedState={}) => (
-    createStore(
-        RootReducer,
-=======
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
@@ -18,7 +7,6 @@ import rootReducer from '../reducers/root_reducer';
 const configureStore = (preloadedState = {}) => (
     createStore(
         rootReducer,
->>>>>>> 40da76dd655bf920afcd21b2595bc0b4b352a821
         preloadedState,
         applyMiddleware(thunk, logger)
     )
