694f977c5c60df68bd940e193d8bdfd4f5e381a1
aliashafi <alia_shafi@alumni.brown.edu>
2019-09-25 08:42:32 -0700

    created patient add

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
 import PatientReducer from './patient_reducer';
-import ErrorsReducer from './errors_reducer';
+// import ErrorsReducer from './errors_reducer';
 
 
 export default combineReducers({
     patients: PatientReducer,
-    errors: ErrorsReducer,
+    // errors: ErrorsReducer,
 });
