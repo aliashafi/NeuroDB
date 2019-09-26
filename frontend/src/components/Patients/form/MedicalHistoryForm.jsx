cb861a6e7bedaabebdddd7bf90c48f8a13787bd3
aliashafi <alia_shafi@alumni.brown.edu>
2019-09-25 17:02:35 -0700

    cssed demographic page

--- frontend/src/components/Patients/form/MedicalHistoryForm.jsx
+++ frontend/src/components/Patients/form/MedicalHistoryForm.jsx
@@ -8,90 +8,96 @@ class MedicalHistoryForm extends React.Component {
     }
 
     render(){
-        if (this.props.currentStep !== "medicalHistory"){
+        if (this.props.currentStep !== "medical history"){
             return null
         }
         return(<div className="card__sub-form">
+        <div className="patient-dem-container">
 
-            <h1>Medical History</h1>
-            <section>
-                <label className="form__label">BDI
-                    <input 
-                        type="text" 
-                        name="BDI" 
-                        value={this.props.medicalHistory.BDI}
-                        onChange={this.props.handleChange}
-                        id="medicalHistory"
-                        />
-                </label>
             
-                <label className="form__label" >BAI
-                    <input 
-                        type="text" 
-                        name="BAI"
-                        value={this.props.medicalHistory.BAI}
-                        onChange={this.props.handleChange}
-                        id="medicalHistory"/>
-                </label>
-            </section>
+            <h1>Medical History</h1>
+            <div className='header-divider'></div>
+            <div className="patient-show-inner-card bigger flex-row">
+                <section className='inner-card__section-grouping'>
+                    <label className="form__label">BDI
+                        <input 
+                            type="text" 
+                            name="BDI" 
+                            value={this.props.medicalHistory.BDI}
+                            onChange={this.props.handleChange}
+                            id="medicalHistory"
+                            />
+                    </label>
+                
+                    <label className="form__label" >BAI
+                        <input 
+                            type="text" 
+                            name="BAI"
+                            value={this.props.medicalHistory.BAI}
+                            onChange={this.props.handleChange}
+                            id="medicalHistory"/>
+                    </label>
+                </section>
 
-            <section>
+                <section>
 
-                <label className="form__label" >Epilepsy Diagnosis
-                    <input
-                        type="text"
-                        name="birthDate"
-                        value={this.props.medicalHistory.epilepsyDiagnosis}
-                        onChange={this.props.handleChange}
-                        id="medicalHistory" />
-                </label>
-
-                    <label className="form__label__checkbox" >Previous Resection
-                        <div>
-                            <label> Yes
-                                <input
-                                    type="checkbox"
-                                    name="previousResection"
-                                    id="medicalHistory"
-                                    onChange={this.props.handleChange}
-                                    value={true} />
-                            </label>
-                            <label> No
-                                <input
-                                    type="checkbox"
-                                    name="previousResection"
-                                    onChange={this.props.handleChange}
-                                    id="medicalHistory"
-                                    value={false} />
-                            </label>
-                        </div>
+                    <label className="form__label" >Epilepsy Diagnosis
+                        <input
+                            type="text"
+                            name="birthDate"
+                            value={this.props.medicalHistory.epilepsyDiagnosis}
+                            onChange={this.props.handleChange}
+                            id="medicalHistory" />
                     </label>
 
-
-                    <label className="form__label__checkbox" >NeuroPace
-                        <div>
-                            <label> Yes
-                                <input
-                                    type="checkbox"
-                                    name="neuroPace"
-                                    id="medicalHistory"
-                                    onChange={this.props.handleChange}
-                                    value={true} />
-                            </label>
-                            <label> No
-                                <input
-                                    type="checkbox"
-                                    name="neuroPace"
-                                    onChange={this.props.handleChange}
-                                    id="medicalHistory"
-                                    value={false} />
-                            </label>
-                        </div>
-                    </label>
-
-            </section>
+                        <label className="form__label__checkbox" >Previous Resection
+                            <div>
+                                <label> Yes
+                                    <input
+                                        type="checkbox"
+                                        name="previousResection"
+                                        id="medicalHistory"
+                                        onChange={this.props.handleChange}
+                                        value={true} />
+                                </label>
+                                <label> No
+                                    <input
+                                        type="checkbox"
+                                        name="previousResection"
+                                        onChange={this.props.handleChange}
+                                        id="medicalHistory"
+                                        value={false} />
+                                </label>
+                            </div>
+                        </label>
 
 
+                        <label className="form__label__checkbox" >NeuroPace
+                            <div>
+                                <label> Yes
+                                    <input
+                                        type="checkbox"
+                                        name="neuroPace"
+                                        id="medicalHistory"
+                                        onChange={this.props.handleChange}
+                                        value={true} />
+                                </label>
+                                <label> No
+                                    <input
+                                        type="checkbox"
+                                        name="neuroPace"
+                                        onChange={this.props.handleChange}
+                                        id="medicalHistory"
+                                        value={false} />
+                                </label>
+                            </div>
+                        </label>
+
+                </section>
+            </div>
+
+
+        </div>
         </div>)
         
     }
