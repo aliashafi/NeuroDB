8c594b8d5bb78464497b9f6206c5576f444e6509
ernestman <ernestjunman@gmail.com>
2019-09-27 11:59:18 -0700

    minor styling for user show

--- frontend/src/components/User/user_show.jsx
+++ frontend/src/components/User/user_show.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../css/patient_show.scss";
 
 class UserShow extends React.Component {
     constructor(props) {
@@ -27,14 +28,27 @@ class UserShow extends React.Component {
 
 
         return (
-            <div>
-                <h1>Welcome, {currentUser.firstName} {currentUser.lastName[0]}.</h1>
-                <h2>{currentUser.email}</h2>
-                <ul>{currentUser.affiliation}</ul>
-                <ul>is Admin: {currentUser.isAdmin.toString()}</ul>
-                <ul>is Verified: {currentUser.isVerified.toString()}</ul>
-                <div>
-                    Pending users: {myPendingUsers}
+            <div className="patient-dem-container">
+                <h1 className="patient-show-inner-card__header initial-header">
+                    Welcome, {currentUser.firstName} {currentUser.lastName[0]}.
+                </h1>
+                <div className="header-divider"></div>
+                <div className="inner-card__section-grouping">
+                    <div className="inner-card__field-grouping flex-size">
+                        <div className="inner-card__field-label">Email address</div>
+                        <div className="inner-card__field-value">{currentUser.email}</div>
+                    </div>
+                    <div className="inner-card__field-grouping flex-size">
+                        <div className="inner-card__field-label">Affiliation</div>
+                        <div className="inner-card__field-value">{currentUser.affiliation}</div>
+
+                    </div>
+                    <div className="inner-card__field-grouping flex-size">
+                        <div className="inner-card__field-label">Pending users</div> 
+                        <div className="inner-card__field-value">{myPendingUsers}</div>
+                    </div>
+                    <ul>is Admin: {currentUser.isAdmin.toString()}</ul>
+                    <ul>is Verified: {currentUser.isVerified.toString()}</ul>
                 </div>
 
             </div>
