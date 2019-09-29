72932d43e30242ffb17f98e12c368bca750b42d6
aliashafi <alia_shafi@alumni.brown.edu>
2019-09-28 19:42:20 -0700

    As ftr search index (#46)
    * edited search
    
    * edited search feature

--- frontend/src/components/Patients/PatientIndex.jsx
+++ frontend/src/components/Patients/PatientIndex.jsx
@@ -1,147 +1,84 @@
 import React from 'react';
-import '../../css/patient_index.scss';
+import '../../css/patient_table.scss'
 // import PatientIndexItem from './PatientIndexItem';
 import PatientIndexSideBar from './PatientIndexSubcomp/PatientIndexSideBar';
 import PatientIndexQuickView from './PatientIndexSubcomp/PatientIndexQuickView';
 import PatientTable from './PatientIndexSubcomp/PatientTable';
 
 import navBar from '../nav_bar';
+import { Agent } from 'https';
 
 class PatientIndex extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            component: '',
-            quickView: '',
             patients: [],
-            sortStatus: 'unsorted',
-            arrow: '>'
-        };
-        this.handleClick = this.handleClick.bind(this);
-        this.handleQuickView = this.handleQuickView.bind(this);
-        this.sortBy = this.sortBy.bind(this);
-        this.sortByDemographics = this.sortByDemographics.bind(this);
+            slide: "false"
+        }
 
-        this.closeQuickView = this.closeQuickView.bind(this);
+        this.toggleSlide = this.toggleSlide.bind(this)
+        this.updatePatientsWithFilter = this.updatePatientsWithFilter.bind(this);
     }
 
     componentDidMount() {
         this.props.fetchPatients();
     }
 
-    handleClick() {
-        if (this.state.component === '') {
-            this.setState({component: <PatientIndexSideBar sortBy={this.sortBy} sortByDemographics={this.sortByDemographics}/>})
-        } else {
-            this.setState({component: ''})
-        }
-        if (this.state.arrow === '>') {
-            this.setState({arrow: '<'})
-        } else {
-            this.setState({arrow: '>'})
-        }
+    updatePatientsWithFilter(filteredPatients){
+        this.setState({
+            patients: filteredPatients
+        })
     }
 
-    handleQuickView(index) {
-        this.setState({quickView: <PatientIndexQuickView patient={this.props.patients[index]} closeQuickView={this.closeQuickView}/>})
-    }
-
-    closeQuickView() {
-        this.setState({quickView: ''})
-    }
-
-    sortBy(key) {
-        let patients = this.props.patients;
-        if (this.state.sortStatus === 'unsorted' || this.state.sortStatus === 'desc') {
-            this.setState({
-                patients: patients.sort(function(a, b) { 
-                    if (a[key] < b[key]) {
-                        return -1
-                    } if (a[key] > b[key]) {
-                        return 1
-                    } else {
-                        return 0
-                    }
-                })
-            })
-            this.setState({
-                sortStatus: 'asc'
-            })
-        } else {
-            this.setState({
-                patients: patients.sort(function(a, b) {
-                    if (b[key] < a[key]) {
-                        return -1
-                    } if (b[key] > a[key]) {
-                        return 1
-                    } else {
-                        return 0
-                    }
-                })
-            })
-            this.setState({
-                sortStatus: 'desc'
-            })
+    toggleSlide(){
+        if (this.state.slide){
+            this.openNav();
+        }else{
+            this.closeNav();
         }
 
+        this.setState({
+            slide: !this.state.slide
+        })
     }
 
-    sortByDemographics(key) {
-        let patients = this.props.patients;
-        if (this.state.sortStatus === 'unsorted' || this.state.sortStatus === 'desc') {
-            this.setState({
-                patients: patients.sort(function(a, b) { 
-                    if (a.demographics[key] < b.demographics[key]) {
-                        return -1
-                    } if (a.demographics[key] > b.demographics[key]) {
-                        return 1
-                    } else {
-                        return 0
-                    }
-                })
-            })
-            this.setState({
-                sortStatus: 'asc'
-            })
-        } else {
-            this.setState({
-                patients: patients.sort(function(a, b) {
-                    if (b.demographics[key] < a.demographics[key]) {
-                        return -1
-                    } if (b.demographics[key] > a.demographics[key]) {
-                        return 1
-                    } else {
-                        return 0
-                    }
-                })
-            })
-            this.setState({
-                sortStatus: 'desc'
-            })
-        }
+    openNav() {
+        document.querySelector(".side-nav-container-index").style.width = "300px";
+        // document.querySelector(".hold-advanced-search").style.marginLeft = "250px";
+    }
+
+    closeNav() {
+        document.querySelector(".side-nav-container-index").style.width = "0";
+        document.querySelector(".hold-advanced-search").style.marginLeft = "0";
     }
 
     render() {
+        const patients = this.state.patients.length === 0 ? this.props.patients : this.state.patients
+        console.log(this.state.patients)
         return (
-            <div className='index-page-main-container'>
-                {this.state.component}
+            <div className="hold-advanced-search">
+                {/* {this.state.component} */}
 
-                <button className='sidebar-trigger' onClick={this.handleClick}>
-                    {this.state.arrow}
-                </button>
-                <div className='patient-index-main'>
-                    <div className='patient-index-main-body'>
-                        <div className='patients-title'>
-                            PATIENTS
-                        </div>
-                        <div className='header-divider'></div>
-                        <div className='patient-index-item-container'>
-                            <PatientTable patients={this.props.patients} handleQuickView={this.handleQuickView}/>
-                        </div> 
+                <PatientIndexSideBar 
+                    patients={this.props.patients}
+                    updatePatientsWithFilter={this.updatePatientsWithFilter}
+                    />
+                
+                <div className="patient-index-page">
+                    <div className="patient-index-page__headers">
+                        <h2>NeuroDB Database</h2>
+                        <h3>All Patients</h3>
                     </div>
-                </div>
 
-                {this.state.quickView}
+                    
+                    <PatientTable 
+                        patients={patients} 
+                        handleQuickView={this.handleQuickView}
+                        toggleSlide={this.toggleSlide}
+                        />
+                    
+                    {/* {this.state.quickView} */}
+                </div>
                 
             </div>
         )
--- frontend/src/components/Patients/PatientIndexSubcomp/PatientTable.jsx
+++ frontend/src/components/Patients/PatientIndexSubcomp/PatientTable.jsx
@@ -1,140 +1,70 @@
 import React from 'react';
-import { EventEmitter } from 'events';
+import { sortByDate, sortByAge, sortByResearchID} from '../../../util/sort_api_util'
+import '../../../css/patient_table.scss'
+import '../../../css/_tackons.scss'
+import PatientBasicSearchBar from './PatientBasicSearchBar';
 
 class PatientTable extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            filteredPatients: []
+            patients: [],
         }
-        this.handleRIDSearch = this.handleRIDSearch.bind(this);
-        this.handlePIDSearch = this.handlePIDSearch.bind(this);
-        this.handleGenderSearch = this.handleGenderSearch.bind(this);
-        this.handleAgeSearch = this.handleAgeSearch.bind(this);
+
+        this.updateTablePatients = this.updateTablePatients.bind(this);
+       
     }
 
-    componentDidMount() {
-        this.setState({
-            filteredPatients: this.props.patients
-        })
-    }
+    updateTablePatients(newPatients){
+        this.setState({patients: newPatients})
+    }    
 
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            filteredPatients: nextProps.patients
-        })
-    }
-
-    handleRIDSearch(event) {
-        event.preventDefault();
-        let currentList;
-        let newList;
-        if (event.target.value !== '') {
-            currentList = this.state.filteredPatients;
-            newList = currentList.filter(patient => {
-                let lowercased = patient.researchId.toLowerCase();
-                let filtered = event.target.value.toLowerCase();
-                return lowercased.includes(filtered);
-            })
-        } else {
-            newList = this.props.patients
+    render() {        
+        let patients = this.props.patients;
+        if (this.state.patients.length > 0){
+            patients = this.state.patients
         }
-        this.setState({
-            filteredPatients: newList
-        })
-    }
-
-    handlePIDSearch(event) {
-        let currentList;
-        let newList;
-        if (event.target.value !== '') {
-            currentList = this.state.filteredPatients;
-            newList = currentList.filter(patient => {
-                let lowercased = patient._id.toLowerCase();
-                let filtered = event.target.value.toLowerCase();
-                return lowercased.includes(filtered);
-            })
-        } else {
-            newList = this.props.patients
-        }
-        this.setState({
-            filteredPatients: newList
-        })
-    }
-
-    handleGenderSearch(event) {
-        let currentList;
-        let newList;
-        if (event.target.value !== '') {
-            currentList = this.state.filteredPatients;
-            newList = currentList.filter(patient => {
-                let lowercased = patient.demographics.gender.toLowerCase();
-                let filtered = event.target.value.toLowerCase();
-                return lowercased.includes(filtered);
-            })
-        } else {
-            newList = this.props.patients
-        }
-        this.setState({
-            filteredPatients: newList
-        })
-    }
-
-    handleAgeSearch(event) {
-        let currentList;
-        let newList;
-        if (event.target.value !== '') {
-            currentList = this.state.filteredPatients;
-            newList = currentList.filter(patient => {
-                let lowercased = patient.demographics.age.toString();
-                let filtered = event.target.value
-                return lowercased.includes(filtered);
-            })
-        } else {
-            newList = this.props.patients
-        }
-        this.setState({
-            filteredPatients: newList
-        })
-    }
-
-    render() {
 
         return (
-            <div>
-                <div className='search-container'>
-                    Search by
-                    <input className='search-box' type="text" placeholder='Patient ID' onChange={this.handlePIDSearch}/>
-                    <input className='search-box' type="text" placeholder='Research ID' onChange={this.handleRIDSearch}/>
-                    <input className='search-box' type="text" placeholder='Age' onChange={this.handleAgeSearch}/>
-                    <input className='search-box' type="text" placeholder='Gender (M/F)' onChange={this.handleGenderSearch}/>
+            <div className="patient-table-page">
+                <div className="patient-table-card patient-table-container std-shadow" id="no-padding">
+
+                <PatientBasicSearchBar 
+                    toggleSlide={this.props.toggleSlide}
+                    patients={this.props.patients} 
+                    updateTablePatients={this.updateTablePatients}/>
+                
                 </div>
                 
-                <div className='patient-table-main-container'>
-                    <div className='patient-table-headers'>
-                        <div className='table-header1'>Research ID</div>
-                        <div className='table-header2'>Patient ID</div>
-                        <div className='table-header3'>Gender</div>
-                        <div className='table-header4'>Age</div>
-                        <div className='table-header5'>Dominant hand</div>
-                        <div className='table-header6'>Native language</div>
-                        <div className='table-header7'>Date of surgery</div>
+                <div className=" patient-table-card patient-table-container std-shadow">
+                <div className="patient-table">
+
+                    <div className="patient-table__headers">
+                            <div>Research ID</div>
+                            <div>Date of surgery</div>
+                            <div>Age</div>
+                            <div>Gender</div>
+                            <div>Coverage (L/R)</div>
+                            <div>Native Language</div>
                     </div>
+
                     <div>
-                        {this.state.filteredPatients.map((patient, index) => (
-                            <div  className='patient-table-body' onClick={() => this.props.handleQuickView(this.props.patients.indexOf(this.state.filteredPatients[index]))}>
-                                <div className='table-text1'>{patient.researchId}</div>
-                                <div className='table-text2'>{patient._id}</div>
-                                <div className='table-text3'>{patient.demographics.gender}</div>
-                                <div className='table-text4'>{patient.demographics.age}</div>
-                                <div className='table-text5'>{patient.demographics.dominantHand}</div>
-                                <div className='table-text6'>{patient.demographics.nativeLanguage}</div>
-                                <div className='table-text7'>{patient.dateOfSurgery}</div>
+                        {patients.map((patient, index) => (
+                            <div 
+                            className="patient-table__rows">
+                                <div>{patient.researchId}</div>
+                                <div>{patient.dateOfSurgery}</div>
+                                <div>{patient.demographics.age}</div>
+                                <div>{patient.demographics.gender}</div>
+                                <div>{patient.demographics.dominantHand}</div>
+                                <div>{patient.demographics.nativeLanguage}</div>
                             </div>
+
                         ))}
                     </div>
+                
                 </div>
-
+                </div>
             </div>
         )
     }
