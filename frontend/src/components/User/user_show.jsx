eb03fb701ea17799168850976327d96df07dd275
aliashafi <alia_shafi@alumni.brown.edu>
2019-11-07 21:24:47 -0800

    merging latest changes (#59) (#61)
    * latest
    
    * Fix patient create route typo on nav menu
    
    * Development (#57)
    
    * Commit root.js file that keeps needing to be comitted without any change
    
    * Fix patient create route typo on nav menu
    
    * As ftr patient pop up (#56)
    
    * latest
    
    * added popup page
    
    * styling for user show (#58)
    
    * fixed root
    
    * Update README.md
    
    * fixed signup
    
    * fixed login
    
    * fixed user login and added demo user
    
    * changed title
    
    * added

--- frontend/src/components/User/user_show.jsx
+++ frontend/src/components/User/user_show.jsx
@@ -22,18 +22,17 @@ class UserShow extends React.Component {
         return (event) => {
             event.preventDefault()
             const adminId = this.props.currentUser._id
-            // debugger
             this.props.verifyToken(token, adminId)
-            // user = pendUserEmail
-            // this.props.updateCurrentUserPendings(user, adminId)
+            
         }
     }
 
     render() {
         
-        // debugger
+        let unVerified = Object.values(this.props.users).filter(user => !user.isVerified)
+        
         const {currentUser} = this.props;
-        // debugger
+        
         
         if (!currentUser) {
             return null;
@@ -46,18 +45,36 @@ class UserShow extends React.Component {
             const month = months[joinDate.getMonth()];
             const year = joinDate.getFullYear().toString();
         
-            const myPendingUsers = Object.keys(currentUser.pendingUsers).map( (user, i) => {
-                const token = this.props.currentUser.pendingUsers[user]
+            const myPendingUsers = unVerified.map( (user, i) => {
+                // const token = this.props.currentUser.pendingUsers[user]
                 return (   
                     <div className="pend-item" key={i}>
+<<<<<<< HEAD
+                        <span><div id="verify-user">{user.firstName} {user.lastName}</div></span>
+                        <span><div id="verify-user">{user.affiliation}</div></span>
+                        <span><button id="verify-btn">Verify</button></span>
+=======
                         <span><div id="verify-user">{user}</div></span>
                         <span><button id="verify-btn" onClick={this.handleVerify(token, user)}>Verify</button></span>
+>>>>>>> c5f3e16150864dab38de4c12e7bf333accd5be31
                     </div>
                 )
             })
 
             const pendingUserDisplay = currentUser.isAdmin ? (
                 <div className="user-pend">
+<<<<<<< HEAD
+                    <h3>All Users</h3>
+                    <div className="user-pending-headers">
+                        <h2>Name</h2>
+                        <h2>Affiliation</h2>
+                        <h2>Status</h2>
+                    </div>
+                    <div className="pend-list">{myPendingUsers}</div>
+                </div>
+            ) : (
+               ""
+=======
                     <h3>Pending approvals</h3>
                     <div className="pend-list">{myPendingUsers}</div>
                 </div>
@@ -66,6 +83,7 @@ class UserShow extends React.Component {
                     <h1>Approval Status</h1>
                         <h2>Approved</h2>
                 </div>
+>>>>>>> c5f3e16150864dab38de4c12e7bf333accd5be31
             )
 
             return (
@@ -104,7 +122,10 @@ class UserShow extends React.Component {
                                 </div>
                             </div>
                             <div className="user-pending-container">
-                                {pendingUserDisplay}
+                                <div className="task-card">
+                                    <h2>Tasks</h2>
+                                </div>
+                                {/* {pendingUserDisplay} */}
                             </div>
                         </div>
                     </div>
